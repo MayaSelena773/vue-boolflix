2393f68531781a99a321dc790cbfb705ed5b4f41
MayaSelena773 <maya.bono721@gmail.com>
2022-07-13 17:30:01 +0200

    header begin

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,30 @@
+<template>
+<header>
+    <div>BOOLFLIX</div>
+
+    <div>
+        <input placeholder="scrivi un titolo"></input>
+        <button type="primary">cerca</button>
+    </div>
+
+</header>
+</template>
+
+<script>
+export default {
+    name:'PageHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+
+header {
+    background-color: lightblue;
+    display: flex;
+    justify-content: space-between;
+    height: 20px;
+}
+
+</style>
+
+
